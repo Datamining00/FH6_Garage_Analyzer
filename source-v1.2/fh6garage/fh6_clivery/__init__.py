3eaa14b4a9bc1023104c932645efc0ba338595a0
AAA <49602959+Datamining00@users.noreply.github.com>
2026-08-21 03:27:49 +0900

    Update independent decoder exports

--- source-v1.2/fh6garage/fh6_clivery/__init__.py
+++ source-v1.2/fh6garage/fh6_clivery/__init__.py
@@ -1,6 +1,16 @@
-"""Independent, read-only FH6 C_livery decoding boundary."""
+"""Independent, read-only FH6 C_livery and C_group decoding boundary."""
 
 from .container import CliveryDecodeError, ContainerInfo, inflate_clivery
+from .cgroup import (
+    CGROUP_FORMAT_ID,
+    CGroupContainerInfo,
+    CGroupDecodeError,
+    CGroupScene,
+    decode_cgroup_bytes,
+    decode_cgroup_file,
+    decode_cgroup_file_to_json,
+    inflate_cgroup,
+)
 from .decoder import (
     FORMAT_ID,
     SECTION_NAMES,
@@ -10,16 +20,32 @@ from .decoder import (
     decode_clivery_file,
     decode_clivery_file_to_json,
 )
+from .records import RawRecord, SourceSpan, Transform
+from .scene import GroupNode, ShapeNode, UnknownNode
 
 __all__ = [
+    "CGROUP_FORMAT_ID",
+    "CGroupContainerInfo",
+    "CGroupDecodeError",
+    "CGroupScene",
     "CliveryDecodeError",
-    "ContainerInfo",
     "CliveryMilestone1",
+    "ContainerInfo",
     "FORMAT_ID",
+    "GroupNode",
+    "RawRecord",
     "SECTION_NAMES",
     "SectionCount",
+    "ShapeNode",
+    "SourceSpan",
+    "Transform",
+    "UnknownNode",
+    "decode_cgroup_bytes",
+    "decode_cgroup_file",
+    "decode_cgroup_file_to_json",
     "decode_clivery_bytes",
     "decode_clivery_file",
     "decode_clivery_file_to_json",
+    "inflate_cgroup",
     "inflate_clivery",
 ]
