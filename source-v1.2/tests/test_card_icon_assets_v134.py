4f3b78f43067695c40d655a208501d54171fdffb
AAA <49602959+Datamining00@users.noreply.github.com>
2026-08-28 15:54:38 +0900

    Extend card icon regression set for metadata arrows

--- source-v1.2/tests/test_card_icon_assets_v134.py
+++ source-v1.2/tests/test_card_icon_assets_v134.py
@@ -20,6 +20,7 @@ class CardIconAssetTests(unittest.TestCase):
             "05_info.png", "06_folder.png", "07_export.png", "08_paint.png",
             "09_unlock.png", "10_lock.png", "11_visible.png", "12_hidden.png",
             "13_circle.png", "14_triangle.png", "15_x.png", "16_import.png",
+            "17_collapse_right.png", "18_expand_left.png",
         }
         self.assertEqual({path.name for path in CARD_ROOT.glob("*.png")}, expected)
 
