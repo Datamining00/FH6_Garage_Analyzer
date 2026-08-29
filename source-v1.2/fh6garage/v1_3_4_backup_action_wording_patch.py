42a0028533818d7cd6c6e929633b34a96b535e30
AAA <49602959+Datamining00@users.noreply.github.com>
2026-08-30 06:07:09 +0900

    Layer v1.4 backup repository and identity patches

--- source-v1.2/fh6garage/v1_3_4_backup_action_wording_patch.py
+++ source-v1.2/fh6garage/v1_3_4_backup_action_wording_patch.py
@@ -22,6 +22,8 @@ from .v1_3_4_card_polish_export_delete_patch import apply_v1_3_4_card_polish_exp
 from .v1_3_4_livery_backup_filter_patch import apply_v1_3_4_livery_backup_filter_patch
 from .v1_3_4_status_backup_label_patch import apply_v1_3_4_status_backup_label_patch
 from .v1_3_4_performance_probe_patch import apply_v1_3_4_performance_probe_patch
+from .v1_4_backup_repository_patch import apply_v1_4_backup_repository_patch
+from .v1_4_identity_patch import apply_v1_4_identity_patch
 
 install_performance_measurement_guard(_performance_metrics)
 
@@ -100,4 +102,6 @@ def apply_v1_3_4_backup_action_wording_patch(MainWindow: Any) -> None:
     apply_v1_3_4_card_polish_export_delete_patch(MainWindow)
     apply_v1_3_4_livery_backup_filter_patch(MainWindow)
     apply_v1_3_4_status_backup_label_patch(MainWindow)
+    apply_v1_4_backup_repository_patch(MainWindow)
+    apply_v1_4_identity_patch(MainWindow)
     apply_v1_3_4_performance_probe_patch(MainWindow)
