67683c2f806aba5aff5aad6fa229deabec5547f8
AAA <49602959+Datamining00@users.noreply.github.com>
2026-08-31 00:24:27 +0900

    Wire backup watcher stability guard

--- source-v1.2/fh6garage/v1_3_4_backup_action_wording_patch.py
+++ source-v1.2/fh6garage/v1_3_4_backup_action_wording_patch.py
@@ -24,6 +24,7 @@ from .v1_3_4_status_backup_label_patch import apply_v1_3_4_status_backup_label_p
 from .v1_3_4_performance_probe_patch import apply_v1_3_4_performance_probe_patch
 from .v1_4_backup_repository_patch import apply_v1_4_backup_repository_patch
 from .v1_4_backup_repository_followup_patch import apply_v1_4_backup_repository_followup_patch
+from .v1_4_backup_watch_stability_patch import apply_v1_4_backup_watch_stability_patch
 from .v1_4_identity_patch import apply_v1_4_identity_patch
 from .v1_4_ui_completion_patch import apply_v1_4_ui_completion_patch
 from .v1_4_display_row_geometry_patch import apply_v1_4_display_row_geometry_patch
@@ -114,6 +115,7 @@ def apply_v1_3_4_backup_action_wording_patch(MainWindow: Any) -> None:
     apply_v1_3_4_status_backup_label_patch(MainWindow)
     apply_v1_4_backup_repository_patch(MainWindow)
     apply_v1_4_backup_repository_followup_patch(MainWindow)
+    apply_v1_4_backup_watch_stability_patch(MainWindow)
     apply_v1_4_local_app_data_patch(MainWindow)
     apply_v1_4_identity_patch(MainWindow)
     apply_v1_4_ui_completion_patch(MainWindow)
