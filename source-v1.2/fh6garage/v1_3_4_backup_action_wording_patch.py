b7eb9d96dcab5a5283693b77ea38bcf50209ec5f
AAA <49602959+Datamining00@users.noreply.github.com>
2026-08-30 14:37:47 +0900

    Install final v1.4 UI completion layer

--- source-v1.2/fh6garage/v1_3_4_backup_action_wording_patch.py
+++ source-v1.2/fh6garage/v1_3_4_backup_action_wording_patch.py
@@ -25,6 +25,7 @@ from .v1_3_4_performance_probe_patch import apply_v1_3_4_performance_probe_patch
 from .v1_4_backup_repository_patch import apply_v1_4_backup_repository_patch
 from .v1_4_backup_repository_followup_patch import apply_v1_4_backup_repository_followup_patch
 from .v1_4_identity_patch import apply_v1_4_identity_patch
+from .v1_4_ui_completion_patch import apply_v1_4_ui_completion_patch
 
 install_performance_measurement_guard(_performance_metrics)
 
@@ -106,4 +107,5 @@ def apply_v1_3_4_backup_action_wording_patch(MainWindow: Any) -> None:
     apply_v1_4_backup_repository_patch(MainWindow)
     apply_v1_4_backup_repository_followup_patch(MainWindow)
     apply_v1_4_identity_patch(MainWindow)
+    apply_v1_4_ui_completion_patch(MainWindow)
     apply_v1_3_4_performance_probe_patch(MainWindow)
