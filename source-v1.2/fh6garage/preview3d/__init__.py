14b807079dd1088172d9a462c906e99fcb4a48f8
AAA <49602959+Datamining00@users.noreply.github.com>
2026-09-08 17:26:18 +0900

    Enable native transform-chain tire assembly

--- source-v1.2/fh6garage/preview3d/__init__.py
+++ source-v1.2/fh6garage/preview3d/__init__.py
@@ -1,5 +1,7 @@
 """FinalVerify1 ErrorFix1 3D livery rendering backend for FH6 Assistant."""
 
 from .geometry_integrity_patch import install_geometry_integrity_patch
+from .native_transform_chain_patch import install_native_transform_chain_patch
 
 install_geometry_integrity_patch()
+install_native_transform_chain_patch()
