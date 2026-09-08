f49e3dbfa27ea09565bd81bdc915dfbc8896c54d
AAA <49602959+Datamining00@users.noreply.github.com>
2026-09-08 16:37:11 +0900

    Install global 3D geometry integrity rules

--- source-v1.2/fh6garage/preview3d/__init__.py
+++ source-v1.2/fh6garage/preview3d/__init__.py
@@ -1,1 +1,5 @@
 """FinalVerify1 ErrorFix1 3D livery rendering backend for FH6 Assistant."""
+
+from .geometry_integrity_patch import install_geometry_integrity_patch
+
+install_geometry_integrity_patch()
