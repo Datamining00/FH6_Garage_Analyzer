d305d28a9b18f4b08193db680763b97b2754df43
AAA <49602959+Datamining00@users.noreply.github.com>
2026-09-08 17:54:31 +0900

    Pin transform-chain KFPS helper hash

--- source-v1.2/fh6garage/preview3d/wheel_morph_helper.py
+++ source-v1.2/fh6garage/preview3d/wheel_morph_helper.py
@@ -5,8 +5,8 @@ from pathlib import Path
 import sys
 
 WHEEL_MORPH_HELPER_FILENAME = "Kfps.ChassisConverter.WheelMorph.exe"
-WHEEL_MORPH_HELPER_SHA256 = "c23c6e2b5ee0c81e58f5ed917ea6b1548fd5223f53fc257328ab85f7a9fc933d"
-WHEEL_MORPH_HELPER_REVISION = "kfps_6f53ca3_w3_patch_v2"
+WHEEL_MORPH_HELPER_SHA256 = "f51bb233514088060fb3ee931612612978c2c76496dba947dbeca42dcbf9375a"
+WHEEL_MORPH_HELPER_REVISION = "kfps_6f53ca3_w3_transform_chain_v1"
 
 
 class WheelMorphHelperError(RuntimeError):
