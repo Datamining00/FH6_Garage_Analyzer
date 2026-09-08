2c687b5e7e8639aa39a531e23fc2309f17e8ea13
AAA <49602959+Datamining00@users.noreply.github.com>
2026-09-08 21:07:06 +0900

    Pin scene-parent mechanical attachment helper

--- source-v1.2/fh6garage/preview3d/wheel_morph_helper.py
+++ source-v1.2/fh6garage/preview3d/wheel_morph_helper.py
@@ -5,8 +5,8 @@ from pathlib import Path
 import sys
 
 WHEEL_MORPH_HELPER_FILENAME = "Kfps.ChassisConverter.WheelMorph.exe"
-WHEEL_MORPH_HELPER_SHA256 = "05ea92029368924e80d69273441491d5a6c5abb674e38553aedcd4ef26c47956"
-WHEEL_MORPH_HELPER_REVISION = "kfps_6f53ca3_w3_transform_chain_v3"
+WHEEL_MORPH_HELPER_SHA256 = "6c31333c9016320a965a406ddd3d3b263d21a66a264df9e849fc9fd4b98152b7"
+WHEEL_MORPH_HELPER_REVISION = "kfps_6f53ca3_w3_scene_parent_attachment_v4"
 
 
 class WheelMorphHelperError(RuntimeError):
